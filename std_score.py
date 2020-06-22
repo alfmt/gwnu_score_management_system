e64124e2c040e22f642b6ca4ff7538b2e83654cb
Min Seok Moon <tjr8268@gmail.com>
2020-06-22 23:29:27 +0900

    완성

--- std_score.py
+++ std_score.py
@@ -1,22 +1,18 @@
 class Score:
     total = []
     avg = []
+    rank = []
+    context = []
 
     def open_file(self):
         fp = open("score.csv", "r", encoding="utf-8")
-        context = fp.readlines()
+        self.context = fp.readlines()
         fp.close()
-        return context
+        return
     
     def get_score(self):
-        context = self.open_file()
-        context = Score.open_file(0)
-        student1 = context[0]
-        std_list = student1.split(",")
-        korean = std_list[2]
-        print(korean)
-        for i in range(len(context)):
-            student = context[i].split(',')
+        for i in range(len(self.context)):
+            student = self.context[i].split(',')
             korean = int(student[2])
             english = int(student[3])
             math = int(student[4])
@@ -24,5 +20,23 @@ class Score:
             self.avg.append(round(self.total[i] / 3, 2))
         return
 
+    def write_file(self):
+        for score1 in self.total:
+            r = 1
+            for score2 in self.total:
+                if score1 < score2:
+                    r += 1
+            self.rank.append(r)
+
+        with open("result.csv", "w", encoding="utf-8") as wf:
+            for i in range(len(self.context)):
+                wf.write(self.context[i].replace('\n','') + ',')
+                wf.write(self.total[i].__str__() + ',')
+                wf.write(self.avg[i].__str__() + ',')
+                wf.write(self.rank[i].__str__() + '\n')
+        return
+
 a = Score()
+a.open_file()
 a.get_score()
+a.write_file()
--- test_score.py
+++ test_score.py
@@ -1,7 +1,19 @@
 from std_score import Score
 from unittest import TestCase
 
-class Myscore(TestCase):
+class Myscore(TestCase):   
+    
+    def test_openfile_result(self):
+        a = Score()
+        a.open_file()
+        a.context
+
+        fp = open("score.csv", "r", encoding="utf-8")
+        context_csv = fp.readlines()
+        fp.close()
+
+        self.assertListEqual(a.context, context_csv)
+
     def test_score_resutl(self):
         context = Score.open_file(0)
         student1 = context[0]
